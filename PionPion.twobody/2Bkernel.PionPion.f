627cb7e72f7fffe3902efad45e8eab79baf9bdbf
AlexLong152 <alexlong152@gmail.com>
2024-02-21 11:31:17 -0500

    added diagram C sym part for pionpion scattering length

--- PionPion.twobody/2Bkernel.PionPion.f
+++ PionPion.twobody/2Bkernel.PionPion.f
@@ -2,6 +2,10 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 c     Part of KERNEL code for Twobody Contributions to Few-Nucleon Processes Calculated Via 2N-Density Matrix
 c     NEW Nov 2023: v1.0 Alexander Long/hgrie 
 c               Based on Compton density code v2.0: D. Phillips/A. Nogga/hgrie starting August 2020/hgrie Oct 2022
+c               Documentation for derivation of this kernel can be found in "documentation" folder
+c               and also:
+c               BKM review: https://arxiv.org/pdf/hep-ph/9501384v1.pdf pg115
+c               Deuteron only: https://arxiv.org/abs/hep-ph/0206219v1
 cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 c     CONTAINS SUBROUTINES:
 c              KernelGreeting         : output to stdout with message which process computed and its version number
@@ -33,11 +37,10 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
       integer,intent(in) :: verbosity         ! verbosity index for stdout
 cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
       write(*,*) "--------------------------------------------------------------------------------"
-      write(*,*) "Kernel: Twobody Pion Pion Scattering"
+      write(*,*) "Kernel: Twobody Pion Pion Scattering - pion deuteron scattering length"
       write(*,*) "--------------------------------------------------------------------------------"
       write(*,*) "   Kernel Code Version 1.0"
       write(*,*) "      Alexander Long/hgrie starting December 2023"
-      write(*,*)
       
       if (verbosity.eq.1000) continue
       end
@@ -138,8 +141,8 @@ c     real*8 isospinFactor(0:1,-1:1,0:1,-1:1)
       real*8 pVec(3), ppVec(3)
       real*8 qVec(3)
       real*8 dl12by2
-      complex*16 factorAsym,factorBsym
-      complex*16 factorAasy,factorBasy
+      complex*16 factorAsym,factorBsym,factorCsym
+      complex*16 factorAasy,factorBasy,factorCasy
       real*8 kVec(3)!,q1Vec(3)
       real*8 kpVec(3)
       real*8 mPion, Mnucl,Epi,q0!, Fpi
@@ -147,7 +150,15 @@ c     real*8 isospin(3)
 c     real*8 mu
       real*8 m1,m2,m3,m4
       real*8 reducedMass, prefactor
-c     
+c  
+c   Current scattering length contribution taken from BKM review
+c   DIAGRAM A CONTRIBUTION:
+c   m_\pi^2/(16\pi^4 F_\pi 1+ \mu) 1/(q^2) \tau_1 \cdot \tau_2 - \tau_1^a \tau_2^a
+c   where a is the outgoing quantum number
+
+c   DIAGRAM B CONTRIBUTION
+c   A little unsure on the definition of g_a used in the bkm review
+c   -g_a^2 \delta^{ab}/(32 \pi^4 (1+\mu)) \tau_1 \cdot\tau_2  (q \cdot \vec{sigma}_1)( q \cdot \vec{\sigma}_2)/(q^2 +m_\pi^2)
 ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 c     
 c     Definitions of momenta repeated here for convenience
@@ -177,7 +188,6 @@ c
       ppVec=(/ppx,ppy,ppz/)
       kVec=(/0.d0,0.d0,real(k,8)/)
       mPion=134.976
-      reducedMass=mPion/mNucleon
 
       m1=mNucl
       m2=mPion
@@ -199,32 +209,41 @@ c
       Epi=(mPion**2 + DOT_PRODUCT(kpVec,kpVec))
 
 c     fpi=92.42 defined in constants.def
-c     Define from Beane paper
-      prefactor=(mPion**2)/(32*(1+reducedMass/2)*(Pi*fpi)**4)
-      factorAsym=-1*prefactor*(1/(DOT_PRODUCT(qVec,qVec)))
-      factorBsym=(prefactor/4.d0)*(1/((DOT_PRODUCT(qVec,qVec)+mpion**2)**2))
+c     Define from BKM review 
+      reducedMass=mPion/mNucleon
+      prefactor=1/(32*(1+reducedMass)*(Pi*fpi)**4)
+
+      factorAsym=2*mPion*mPion*prefactor*(1/(DOT_PRODUCT(qVec,qVec)))
+      factorBsym=-1*(gA*gA*prefactor)*(1/((DOT_PRODUCT(qVec,qVec)+mpion**2)))
+      factorCsym=(gA*gA*prefactor)*(1/((DOT_PRODUCT(qVec,qVec)+mpion**2)**2))
+
 
       factorAasy=factorAsym
       factorBasy=factorBsym
+      factorCasy=factorCsym
 
       if ((t12 .eq. t12p) .and. (mt12 .eq. mt12p)) then
 
          if (s12p .eq. s12) then ! spin symmetric part only; s12-s12p=0 => l12-l12p is even
             call CalcKernel2BAsym(Kernel2B,
      &           factorAsym,
-     &           s12p,s12,extQnumlimit,verbosity)
+     &           s12p,s12,t12,extQnumlimit,verbosity)
 
             call CalcKernel2BBsym(Kernel2B,qVec,
-     &           factorAsym,
-     &           s12p,s12,extQnumlimit,verbosity)
-         else                   !  spin anti-symmetric part only; s12 question: s12-s12p=±1 => l12-l12p is odd
-            call CalcKernel2BAasy(Kernel2B,
-     &           factorAasy,
-     &           s12p,s12,extQnumlimit,verbosity)
+     &           factorBsym,
+     &           s12p,s12,t12,extQnumlimit,verbosity)
 
-            call CalcKernel2BBasy(Kernel2B,qVec,
-     &           factorAsym,
-     &           s12p,s12,extQnumlimit,verbosity)
+            call CalcKernel2BCsym(Kernel2B,qVec,
+     &           factorBsym,
+     &           s12p,s12,t12,mt12,extQnumlimit,verbosity)
+c        else                   !  spin anti-symmetric part only; s12 question: s12-s12p=±1 => l12-l12p is odd
+c           call CalcKernel2BAasy(Kernel2B,
+c    &           factorAasy,
+c    &           s12p,s12,extQnumlimit,verbosity)
+
+c           call CalcKernel2BBasy(Kernel2B,qVec,
+c    &           factorAsym,
+c    &           s12p,s12,extQnumlimit,verbosity)
 
          end if                 ! s12 question
 c     else                      ! t12!=t12p
--- PionPion.twobody/2Bspinsym.PionPion.f
+++ PionPion.twobody/2Bspinsym.PionPion.f
@@ -27,7 +27,7 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 c     
       subroutine CalcKernel2BAsym(Kernel2B,
      &     factor,
-     &     Sp,S,extQnumlimit,verbosity)
+     &     Sp,S,t12,extQnumlimit,verbosity)
 c     
 c********************************************************************
 c     
@@ -48,7 +48,7 @@ c********************************************************************
 c     INPUT VARIABLES:
 c     
       complex*16,intent(in) :: factor
-      integer,intent(in) :: Sp,S
+      integer,intent(in) :: Sp,S,t12
       integer,intent(in) :: extQnumlimit
       integer,intent(in) :: verbosity
 c     
@@ -58,23 +58,30 @@ c
 c     complex*16 hold(0:1,-1:1,0:1,-1:1)
       integer Msp,Ms, extQnum
       real*8 tmp,tmp2, ddelta
-     
-c     hold = identity in this case since theres no explicit spin depedence, only isospin
-      do extQnum=1,3
+      real*8 isospin
+
+c     isospin part is 
+c     <t_12p,mt12p|\vec{\tau}_1 \cdot \vec{\tau}_2 - tau_1^a \tau_2^a|t_12, mt_12>
+c     =2*((-1)**(2*t12+1)) \delta_{t12,t12p}, \delta_{mt12,mt12p}
+c     but the isospin deltas are taken care of already.
+
+      isospin=2*((-1)**(2*t12+1))
+      do extQnum=1,3!no dependence on this either
       do Msp=-Sp,Sp
       do Ms=-S,S
             tmp=ddelta(Sp,S)
             tmp2=ddelta(Msp,Ms)
-            Kernel2B(extQnum,Sp,Msp,S,Ms) = Kernel2B(extQnum,Sp,Msp,S,Ms) + factor*tmp*tmp2
+            Kernel2B(extQnum,Sp,Msp,S,Ms) = Kernel2B(extQnum,Sp,Msp,S,Ms) + factor*tmp*tmp2*isospin
       end do
       end do
       end do 
+c     Note the factor 2*((-1)**(2*t12+1)) only appears in the case where t12=t12p and mt12=mt12p but this is taken care
+c     of in 2Bkernel.PionPion.f
 c     
 ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
       if (verbosity.eq.1000) continue
       return
       end
-
       subroutine CalcKernel2BBsym(Kernel2B,q,
      &     factor,
      &     Sp,S,extQnumlimit,verbosity)
@@ -116,6 +123,66 @@ c     hold = identity in this case since theres no explicit spin depedence, only
       do Msp=-Sp,Sp
       do Ms=-S,S
             Kernel2B(extQnum,Sp,Msp,S,Ms) = Kernel2B(extQnum,Sp,Msp,S,Ms) + factor*hold(Sp,Msp,S,Ms)
+      end do
+      end do
+      end do 
+c     
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      if (verbosity.eq.1000) continue
+      return
+      end
+
+      subroutine CalcKernel2BCsym(Kernel2B,q,
+     &     factor,
+     &     Sp,S,t12,extQnumlimit,verbosity)
+c     
+c********************************************************************
+c     
+c     Calculates diagram C
+c     
+c********************************************************************
+c     
+      implicit none
+      include '../common-densities/constants.def'
+c     
+c********************************************************************
+c     INPUT/OUTPUT VARIABLES:
+c     
+      complex*16,intent(inout) :: Kernel2B(1:extQnumlimit,0:1,-1:1,0:1,-1:1)
+c      complex*16 Kernel2Bpx(0:1,-1:1,0:1,-1:1),Kernel2Bpy(0:1,-1:1,0:1,-1:1)
+c     
+c********************************************************************
+c     INPUT VARIABLES:
+c     
+      real*8, intent(in) :: q(3)
+      complex*16,intent(in) :: factor
+      integer,intent(in) :: Sp,S,t12
+      integer,intent(in) :: extQnumlimit
+      integer,intent(in) :: verbosity
+c     
+c********************************************************************
+c     LOCAL VARIABLES:
+c      
+      complex*16 hold(0:1,-1:1,0:1,-1:1), hold2(0:1,-1:1,0:1,-1:1)
+      integer Msp,Ms, extQnum
+      real*8 isospin,tmp,tmp2,ddelta
+c     isospin part is 
+c     <t_12p,mt12p|\vec{\tau}_1 \cdot \vec{\tau}_2 |t_12, mt_12>
+c     =(2 t12(t12+1)-3 )\delta_{t12,t12} \delta_{mt12,mt12p}
+      isospin= (2*t12*(t12+1))-3
+
+      do extQnum=1,3
+      do Msp=-Sp,Sp
+      do Ms=-S,S
+            tmp=ddelta(Sp,S)
+            tmp2=ddelta(Msp,Ms)
+            hold2(Sp,Msp,S,Ms)=tmp*tmp2*isospin
+
+            call doublesigmasym(hold,q(1),q(2),q(3),q(1),q(2),q(3),Sp,S,verbosity)
+            hold=hold*2*mpi*mpi*(-1)**(t12)
+
+            Kernel2B(extQnum,Sp,Msp,S,Ms) = Kernel2B(extQnum,Sp,Msp,S,Ms) +
+     &              factor*(hold(Sp,Msp,S,Ms)+hold2(Sp,Msp,S,Ms))
       end do
       end do
       end do 
