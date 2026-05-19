7f58967e5c2e1deffec2752778647bec7ff01aad
Alexander Long <alexlong152@gmail.com>
2026-05-19 12:22:09 -0400

    Fixed bug where onebody output files didn't include full output

--- PionPhotoProdThresh.onebody/run.sh
+++ PionPhotoProdThresh.onebody/run.sh
@@ -3,8 +3,8 @@ make clean
 make
 # if no arguments then run input.dat
 if [ -z "$1" ]; then
-  ./onebodyvia1Ndensity input.dat
+  ./run.onebodyvia1Ndensity input.dat
 else
   # Argument provided, use input-<arg>.dat
-  ./onebodyvia1Ndensity input-$1.dat
+  ./run.onebodyvia1Ndensity input-$1.dat
 fi
